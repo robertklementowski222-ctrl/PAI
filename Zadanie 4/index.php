e33efe59507b93742b5d74e97297183979df4e20
Robert Sebastian Klementowski <robertklementowski222@gmail.com>
2026-09-15 13:07:13 +0200

    Zadania PHP

---
+++ Zadanie 4/index.php
@@ -0,0 +1,63 @@
+<?php 
+
+//Zad 1
+
+// $a = 5;
+
+// if($a % 2 == 0){
+//     echo "liczba jest parzysta";
+// } else {
+//     echo "liczba jest nie parzysta";
+// }
+
+
+//Zad 2
+
+// $a = 4;
+// $b = 2;
+
+// if($a % $b == 0){
+//     echo "podzielna";
+// } else {
+//     echo "nie podzielna";
+// }
+
+//Zad 3
+
+// $a = 5;
+
+// if(1 <= $a && $a <= 10){
+//     echo "Liczba nalerzy do przedziału 1-10";
+// } elseif(17 <= $a && $a <= 21){
+//     echo "Liczba nalerzy do przedziału 17-21";
+// } else {
+//     echo "liczba nie nalezy do zadnego przedzialu";
+// }
+
+
+//Zad 4
+
+// $a = 5;
+
+// if($a > 0){
+//     echo "Wieksza od zera";
+// }else {
+//     echo "Mniejsza od zera";
+// }
+
+
+//Zad 5
+
+$a = 15;
+
+if($a < 11){
+    echo "Dziecko";
+} elseif($a >= 11 && $a <= 17){
+    echo "nastolatek";
+} elseif($a > 18){
+    echo "dorosły";
+}
+
+
+
+?>
